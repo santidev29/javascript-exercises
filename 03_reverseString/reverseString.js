4ffe5a5dd720aafd5d570677d8bb93099912bf71
santidev29 <82169279+santidev29@users.noreply.github.com>
2022-02-10 12:28:28 +0100

    10-2-2022

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
 const reverseString = function() {
 
+    let string = "Hello"
+
+    let mystring = string.toLowerCase();
+    let mystring2 = mystring.split("")
+    let reverse = mystring2.reverse();
+    let string2 = reverse.join('');
+
+    return string2;
+
 };
 
 // Do not edit below this line
