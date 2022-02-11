9b4237759c8acd31c3c1b390c2a8aa783c412cdd
santidev29 <82169279+santidev29@users.noreply.github.com>
2022-02-11 19:51:21 +0100

    11-2-2022

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,27 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
+
+        const inicial = x;
+        const final = y;
+        let acum = 0
+
+        if (x < y) {
+
+            for (let i = x ; i <= y; i++) {
+            acum += i;
+            }
+
+        } else {
+
+            for (let i = y ; i <= x; i++) {
+            acum += i;
+            }
+            
+            return acum;
+
+        }
 
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
