d441a02b32825291054f3792794a02b1929d2950
santidev29 <82169279+santidev29@users.noreply.github.com>
2022-02-11 17:26:44 +0100

    11-2-2022

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,12 +1,16 @@
-
-const removeFromArray = function(...args) {
-
+const removeFromArray = function (...args) {
+    
     const array = args[0];
     const newArray = [];
 
     array.forEach((item) => {
-
-    }
+        if (!args.includes(item)) {
+            newArray.push(item);
+        }
+    });
+    
+    return newArray;
+};  
 
 // Do not edit below this line
 module.exports = removeFromArray;
